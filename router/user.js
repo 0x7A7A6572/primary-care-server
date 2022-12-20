cae72db4ecf1c59222fe479b3dc30cf2e3b064b6
zzerx <zzerx@qq.com>
2022-12-20 20:08:13 +0800

    新增注册接口

--- router/user.js
+++ router/user.js
@@ -9,7 +9,8 @@ const Response = require("../utils/Response.js");
 const utils = require("../utils/utils");
 // 引入mysql连接池
 const pool = require("../utils/db.js");
-
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 // 用户注册
 router.post("/user/register", async (req, resp) => {
   let {
@@ -31,10 +32,8 @@ router.post("/user/register", async (req, resp) => {
   });
 
   let { error, value } = schema.validate(req.body);
-  if (error) {
-    resp.send(Response.error(400, error));
-    return; // 结束
-  }
+  if (error) return resp.send(Response.error(400, error));
+
   // 查询用户是否已被注册
   let findUserSql = `select shenfenzheng from user where shenfenzheng=?`;
   // 执行添加操作
@@ -43,11 +42,9 @@ router.post("/user/register", async (req, resp) => {
     values 
      (?,?,?,?,?,?,?,?,?,?,?)`;
   try {
-    console.log("??")
     // 查询该用户是否已被注册
     let haved = await utils.query(findUserSql, [shenfenzheng]);
-    console.log("haved",haved)
-   
+
     if (haved.length == 0) {
       // 插入数据
       await utils.query(insertSql, [null, name,
@@ -64,7 +61,46 @@ router.post("/user/register", async (req, resp) => {
 
 // 用户登陆接口
 router.post("/user/login", async (req, resp) => {
-  
-})
+  let { phone, pwdmd5 } = req.body
+  // 表单验证
+  let schema = Joi.object({
+    phone: Joi.string().required().pattern(new RegExp('^\\w{11,18}$')), // 必填
+    pwdmd5: Joi.string().required(), // 必填
+  });
+  let { error, value } = schema.validate(req.body);
+  if (error) return resp.send(Response.error(400, error));
+
+  // 查询数据库，账号密码是否填写正确
+  let checkPwd_sql = "select * from user where (phone=? and pwdmd5=?) or (shenfenzheng=? and pwdmd5=?)";
+  try {
+    let dbres = await utils.query(checkPwd_sql, [phone, pwdmd5, phone, pwdmd5]);
+    if (dbres.length == 0) return resp.send(Response.ok({ code: 400, msg: '账号或密码不正确' }));
+    // 验证成功 生成token
+    let { uid, name } = dbres[0];
+    let payload = { id: uid, name: name };
+    let token = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '1d' }); // 1d用于测试，发布时改成30d
+    resp.send(Response.ok({ user: fllterUserData(dbres[0]), token }));
+  } catch (error) {
+    resp.send(Response.error(500, error));
+  }
+});
+
+
+// 过滤用户信息字段(密码不返回)
+function fllterUserData(u){
+  return {
+    uid: u.uid,
+    name: u.name,
+    phone: u.phone,
+    gender: u.gender,
+    birthday: u.birthday,
+    shenfenzheng: u.shenfenzheng,
+    address: u.address,
+    isdoctor: u.isdoctor,
+    create_time: u.create_time
+  }
+}
+
+
 
 module.exports = router;
