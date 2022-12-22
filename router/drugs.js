309d43fa06867e07c0a5cb4309c07f97223c7936
tudou <1574364721@qq.com>
2022-12-22 19:34:29 +0800

    对于药品查询接口修改

--- router/drugs.js
+++ router/drugs.js
@@ -36,26 +36,40 @@ router.post('/search/drugs',async (req,res)=>{
         res.send(Response.error(error));
       }
     
-// pool.query(sql,[`%${name1}%`,page,pagenum],(err,r)=>{
-
-//     if(err){
-//         res.send(Response.error(500, error));
-//         throw err;
-//     }
-//     res.send(Response.ok(r));
- 
-// })
-
 })
 
 // 查询药品列表
 router.get('/search/drugs/list',async (req,res)=>{
-    let sql="select * from drugs"
-    pool.query(sql,(err,result )=>{
-        if(result.length==0){
-            res.send(Response.error(500, err))
-        }
-            res.send(Response.ok(result));
-    })
+    let {page,pagenum}=req.query;
+    let schema = Joi.object({
+        page: Joi.number().required(), 
+        pagenum: Joi.number().integer().max(100).required(), 
+      })
+      let { error, value } = schema.validate(req.query);
+      if (error) {
+        res.send(Response.error(400, error));
+        return; // 结束
+      }
+      try {
+        let startIndex = (page - 1) * pagenum;
+        let size = parseInt(pagenum);
+        let sql="select * from drugs limit ?,?"
+        let result = await pool.querySync(sql, [startIndex, size]);
+        // 执行查询总条目数
+        let sql2 = "select count(*) as count from drugs";
+        let result2 = await pool.querySync(sql2);
+        let total = result2[0].count;
+        res.send(
+          Response.ok({ page: parseInt(page), pagenum: size, total, result })
+        );
+      } catch (error) {
+        res.send(Response.error(error));
+      }
+    // pool.query(sql,(err,result )=>{
+    //     if(result.length==0){
+    //         res.send(Response.error(500, err))
+    //     }
+    //         res.send(Response.ok(result));
+    // })
 })
 module.exports = router;
