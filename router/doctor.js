9f7c139146d5f9e82bcb01015835eae541c41a5a
zzerx <zzerx@qq.com>
2022-12-30 10:11:23 +0800

    优化医生查询接口和返回数据完整

--- router/doctor.js
+++ router/doctor.js
@@ -16,15 +16,15 @@ router.get('/doctor/search', async (req, res) => {
   if (error) return res.send(Response.error(400, error));
 
   try {
-     let sql = `
+    let sql = `
      select o.*,d.title d_title,h.title h_title,h.address h_addr
         from doctor o
             join depa d on o.did=d.did
         join hospital h on o.hid=h.hid
          where  ( name like ? or good_at like ? )  
-         ${hid ? ' and o.hid=?' : '' } ${did ? ' and o.did=?' : '' }
+         ${hid ? ' and o.hid=?' : ''} ${did ? ' and o.did=?' : ''}
         GROUP BY name desc`;
-    let result = await pool.querySync(sql, [`%${key}%`,`%${key}%`,hid,did]);
+    let result = await pool.querySync(sql, [`%${key}%`, `%${key}%`, hid, did]);
     res.send(Response.ok(result));
   } catch (error) {
     res.send(Response.error(error));
@@ -42,23 +42,55 @@ router.get('/doctor/list', async (req, res) => {
   if (error) return res.send(Response.error(400, error));
   try {
     // let sql = "select * from doctor limit ?,?";
-   let sql = `SELECT o.*,d.title d_title,h.title h_title,h.address h_addr
+    let sql = `SELECT o.*,d.title d_title,h.title h_title,h.address h_addr
     FROM doctor o
         join depa d on o.did=d.did
     join hospital h on o.hid=h.hid
     group by o.name desc 
     limit ?,? ;`;
-    let result = await pool.querySync(sql, [parseInt(page),parseInt(pagenum)]);
-    res.send(Response.ok(result));
+    let count_sql = `select count(*) c from (select count(*)
+                           from doctor
+                       group by name) d;`;
+    let result = await pool.querySync(sql, [(page - 1) * pagenum, parseInt(pagenum)]);
+    let counts = await pool.querySync(count_sql);
+    // console.log({ counts, total: counts[0]?.c, page, pagenum })
+    res.send(Response.ok({ total: counts[0]?.c, page, pagenum, result }));
   } catch (error) {
     res.send(Response.error(error));
   }
 });
 
+// 通过科室和医院查医生
+router.post('/doctor/byHidAndDid',async (req,res)=>{
+  let {hid,did}=req.body;
+  let schema = Joi.object({
+      hid: Joi.number().required(), 
+      did: Joi.number().required(), 
+    })
+    let { error, value } = schema.validate(req.body);
+    if (error) return res.send(Response.error(400, error));
+    try {
+      let sql = `SELECT o.*,d.title d_title,h.title h_title,h.address h_addr
+      FROM doctor o
+          join depa d on o.did=d.did
+      join hospital h on o.hid=h.hid
+      where o.hid=? and o.did=?`;
+      let result = await pool.querySync(sql, [hid,did]);
+      // 执行查询总条目数
+      res.send( Response.ok(result));
+    } catch (error) {
+      res.send(Response.error(error));
+    }
+});
+
 // 专家推荐医生 (暂)
 router.get('/doctor/recommend', async (req, res) => {
   try {
-    let sql = "select * from doctor where doctor.grade = '主任医师' group by name limit 10";
+    let sql = `select o.*,d.title d_title,h.title h_title,h.address h_addr
+    from doctor o
+      join depa d on o.did=d.did
+      join hospital h on o.hid=h.hid
+    where o.grade = '主任医师' group by name limit 10`;
     let result = await pool.querySync(sql, []);
     res.send(Response.ok(result));
   } catch (error) {
